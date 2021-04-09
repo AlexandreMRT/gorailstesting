341350370dbe1567372ca6b305dd6a22359f8c54
Alexandre <mrtalexandre@gmail.com>
2021-04-08 23:33:08 -0300

    Update test to check all number from 0 to 10

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -2,9 +2,11 @@ require "test_helper"
 
 class RatingTest < ActiveSupport::TestCase
   test "is valid when rating is between 0 and 10" do
-    rating = Rating.new(score: 2)
-    rating.valid?
-    assert_empty rating.errors[:score]
+    0.upto(10).each do |number|
+      rating = Rating.new(score: number)
+      rating.valid?
+      assert_empty rating.errors[:score]
+    end
   end
 
   test "is invalid when rating is less than 0" do
