084d22759d8a7aec2d441b3839ae5e4ad9f67264
Alexandre <mrtalexandre@gmail.com>
2021-04-09 23:04:05 -0300

    Add tests for new rating model

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -20,4 +20,65 @@ class RatingTest < ActiveSupport::TestCase
     rating.valid?
     assert_not rating.errors[:score].empty?
   end
+
+  test "promoter? returns true if score is 9 or 10" do
+    rating = Rating.new(score: 9)
+    assert rating.promoter?
+  end
+
+  test "promoter? returns false if score is less than 9" do
+    rating = Rating.new(score: 1)
+    assert_not rating.promoter?
+  end
+
+  test "promoter? is false if score is greater than 10" do
+    rating = Rating.new(score: 11)
+    assert_not rating.promoter?
+  end
+
+  test "passive? returns true if score is 7 or 8" do
+    7.upto(8).each do |number|
+      rating = Rating.new(score: number)
+      rating.passive?
+      assert rating.passive?
+    end
+  end
+
+  test "passive? returns false if score is less than 7" do
+    0.upto(6).each do |number|
+      rating = Rating.new(score: number)
+      rating.passive?
+      assert_not rating.passive?
+    end
+  end
+
+  test "passive? returns false if score is grater than 8" do
+    9.upto(10).each do |number|
+      rating = Rating.new(score: number)
+      rating.passive?
+      assert_not rating.passive?
+    end
+  end
+
+  test "detractor returns true if score is less than or equal to 6" do
+    0.upto(6).each do |number|
+      rating = Rating.new(score: number)
+      rating.detractor?
+      assert rating.detractor?
+    end
+  end
+
+  test "detractor returns false if score is grater than 6" do
+    7.upto(10).each do |number|
+      rating = Rating.new(score: number)
+      rating.detractor?
+      assert_not rating.detractor?
+    end
+  end
+
+  test "detractor returns false if score is less than 0" do
+    rating = Rating.new(score: -1)
+    rating.detractor?
+    assert_not rating.detractor?
+  end
 end
