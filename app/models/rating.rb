3a66e55a211cdb7a59963e3b2877f5b380f52ad0
Alexandre <mrtalexandre@gmail.com>
2021-04-11 23:25:33 -0300

    Add a after create methods for rating to update using score

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,10 @@ class Rating < ApplicationRecord
   belongs_to :person
   validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
+  after_create do
+    person.update(score: score)
+  end
+
   def promoter?
     score >= 9 && score <= 10
   end
