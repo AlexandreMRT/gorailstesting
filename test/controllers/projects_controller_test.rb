c35ed3357c885dec07809ece66a9a6591670e3bd
Alexandre <mrtalexandre@gmail.com>
2021-04-12 23:30:19 -0300

    Add projects controller tests with devise

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -2,28 +2,25 @@ require "test_helper"
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get index" do
+  setup do
+  # sign_in devise helper method
+  sign_in users(:regular)
+
+  end
+
+  test "redirected if not logged in" do
+    sign_out :user
+    get projects_url
+    assert_response :redirect
+  end
+
+  test "can get index" do
     get projects_url
     assert_response :success
   end
 
-  end
-
-  test "should create project" do
-    assert_difference('Project.count') do
-      post projects_url, params: { project: { title: @project.title } }
-    end
-
-    assert_redirected_to project_url(Project.last)
-  end
-
-  test "should show project" do
-    get project_url(@project)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_project_url(@project)
+  test "can get new" do
+    get new_project_url
     assert_response :success
   end
 end
